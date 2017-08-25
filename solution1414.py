8729886feb0d30dbded06b149f5f9497ae0a598c
benpadiyara <bengpadiyara@gmail.com>
2017-08-25 19:01:34 +0530

    14Task Submitted

---
+++ solution1414.py
@@ -0,0 +1,21 @@
+numOne = 2
+numTwo = 3
+
+result = numOne & numTwo
+print(result)
+
+result = numOne | numTwo
+print(result)
+
+result = ~numTwo
+print(result)
+
+result = numTwo ^ numOne
+print(result)
+
+result = numOne << numTwo
+print(result)
+
+result = numOne >> numTwo
+print(result)
+
