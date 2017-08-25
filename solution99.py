f781a0bcea7c5b6058fd1949710ae773c1432ef2
benpadiyara <bengpadiyara@gmail.com>
2017-08-25 16:00:19 +0530

    9Task Submitted

---
+++ solution99.py
@@ -0,0 +1,8 @@
+stringVariable = "Play a game with me if you're a Hacker"
+print(stringVariable[32])                             #Prints a single character of the string
+print(stringVariable[20 :])                           #Prints the sliced string
+print(stringVariable[0 : 11])                        #Prints the sliced string
+print(stringVariable + " You Just Concatinated ! Hacker !")    #Prints the joined string
+print(stringVariable * 3)                            #Prints the string three times
+
+
